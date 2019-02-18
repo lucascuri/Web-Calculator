762eff6817cee4770f85be13371e2d45fd9dcc04
Lucas Curi <lucascurigomes10@gmail.com>
2019-02-18 00:42:09 -0300

    d

---
+++ app.js
@@ -0,0 +1,115 @@
+const s0 = document.querySelector('.s0');
+const s1 = document.querySelector('.s1');
+const s2 = document.querySelector('.s2');
+const s3 = document.querySelector('.s3');
+const s4 = document.querySelector('.s4');
+const s5 = document.querySelector('.s5');
+const s6 = document.querySelector('.s6');
+const s7 = document.querySelector('.s7');
+const s8 = document.querySelector('.s8');
+const s9 = document.querySelector('.s9');
+const s10 = document.querySelector('.s10');
+const s11 = document.querySelector('.s11');
+const s12 = document.querySelector('.s12');
+const s13 = document.querySelector('.s13');
+const s14 = document.querySelector('.s14');
+const s15 = document.querySelector('.s15');
+const s16 = document.querySelector('.s16');
+const s17 = document.querySelector('.s17');
+const s18 = document.querySelector('.s18');
+
+const array = [];
+const operation = () => {
+    
+}
+operation();
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s14.addEventListener('click', function() {
+    array.push(2);
+    s0.innerHTML = '2';
+});
+
+s15.addEventListener('click', function() {
+    array.push(3);
+    s0.innerHTML = '3';
+});
+
+s9.addEventListener('click', function() {
+    array.push(4);
+    s0.innerHTML = '4';
+});
+
+s10.addEventListener('click', function() {
+    array.push(5);
+    s0.innerHTML = '5';
+});
+
+s11.addEventListener('click', function() {
+    array.push(6);
+    s0.innerHTML = '6';
+});
+
+s5.addEventListener('click', function() {
+    array.push(7);
+    s0.innerHTML = '7';
+});
+
+s6.addEventListener('click', function() {
+    array.push(8);
+    s0.innerHTML = '8';
+});
+
+s7.addEventListener('click', function() {
+    array.push(9);
+    s0.innerHTML = '9';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
+
+s13.addEventListener('click', function() {
+    array.push(1);
+    s0.innerHTML = '1';
+});
